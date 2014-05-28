cbaa4ffbc6c342a7bf3c1f225ffb5413fa9104c0
Maimer <nesnl23@yahoo.com>
2014-05-28 09:53:06 -0400

    Fixed blackjack method and added blackjack check to index page

--- blackjack.rb
+++ blackjack.rb
@@ -54,7 +54,7 @@ class Blackjack
     handcheck = []
     handcheck << hand[0].chop
     handcheck << hand[1].chop
-    if handcheck.include?("A") && handcheck.include?("K" || "Q" || "J")
+    if handcheck.include?("A") && handcheck.include?("K", "Q", "J")
       return true
     end
     false
