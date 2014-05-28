d0e72c802f790edc6703771af5242a4553e0ce5e
Brian Cox <briancox730@gmail.com>
2014-05-28 19:32:40 -0400

    Working on JS validation, it doesnt work... I think its because the button/var doesn't exist at time of JS load server side still defaults to 100 though

--- public/js.js
+++ public/js.js
@@ -9,11 +9,13 @@ function validateInitialBet() {
 }
 
 function validateBet() {
-  if (document.homepage_form.initial_bet.value > 0 &&
-    document.homepage_form.initial_bet.value <= <%= @wallet.balance %>) {
+  var balance = document.getElementById("wallet_balance").;
+  var betValue = document.new_bet.bet.value;
+  if (betValue > 0 && betValue <= balance) {
+    document.getElementById("new_bet_warning").innderHTML = "";
     return true;
   } else {
-    document.getElementById("homepage_initial_bet").innerHTML = "*Must be between 0 and <%= @wallet.balance %>";
+    document.getElementById("new_bet_warning").innerHTML = "*Must be between 0 and " + balance;
     return false;
   }
 }
